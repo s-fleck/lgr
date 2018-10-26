56b42e2e31361065667db6e8f559722186dea4d8
Stefan Fleck <stefan.b.fleck@gmail.com>
2018-10-26 23:47:49 +0200

    some updates to appenders and exporting, added a appender_console_minimal, that is faster but cannot be configured

--- R/appenders.R
+++ R/appenders.R
@@ -2,6 +2,17 @@
 #' @include format.R
 #' @include utils.R
 #' @include utils-sfmisc.R
+#'
+#' @export
+appender <- R6::R6Class(
+  "appender",
+  public = list(append = function() NULL )
+)
+
+
+
+
+#' @export
 appender_console <- function(
   x,
   ...,
@@ -12,8 +23,11 @@ appender_console <- function(
 
 
 
+
+#' @export
 appender_file <- R6::R6Class(
   "appender_file",
+  inherit = appender,
   public = list(
     append = NULL,
     file = NULL,
@@ -23,8 +37,11 @@ appender_file <- R6::R6Class(
 
 
 
+
+#' @export
 appender_console <- R6::R6Class(
   "appender_console",
+  inherit = appender,
   public = list(
     initialize = function(
       threshold = NULL,
@@ -45,12 +62,13 @@ appender_console <- R6::R6Class(
       self$colors <- colors
       self$threshold <- threshold
     },
-    append = function(x, ml){
-      threshold <- self$get_threshold(ml)
 
+    append = function(ml){
+      threshold <- self$get_threshold(ml)
+      x <- ml$get_last_row()
 
       if (x$level > threshold){
-        return(invisible())
+        return(invisible(x$msg))
       } else {
         dd <- self$formatter(
           x,
@@ -60,7 +78,8 @@ appender_console <- R6::R6Class(
           ml = ml
         )
       }
-      cat(dd)
+      cat(dd, "\n")
+      return(invisible(x$msg))
     },
 
     get_threshold = function(
@@ -86,9 +105,78 @@ appender_console <- R6::R6Class(
 )
 
 
-app_console_simple <- appender_console$new()
 
-app_console_color <- appender_console$new(
+
+#' @export
+appender_console_minimal <- R6::R6Class(
+  "appender_console",
+  inherit = appender,
+  public = list(
+    initialize = function(
+      threshold = NULL,
+      timestamp_format ="%Y-%m-%d %H:%M:%S"
+    ){
+      assert(
+        is.null(threshold) ||
+        is_scalar_integerish(threshold) ||
+        is_scalar_character(threshold)
+      )
+
+      self$timestamp_format <- timestamp_format
+      self$threshold <- threshold
+    },
+
+    append = function(ml){
+      threshold <- self$get_threshold(ml)
+      x <- ml$get_last_row()
+
+      if (x[[2]] > threshold){
+        return(invisible(x$msg))
+      } else {
+        cat(
+          toupper(ml$label_levels(x[[2]])),
+          " [", format(x[[3]], self$timestamp_format), "] ",
+          x[[5]],
+          "\n",
+          sep = ""
+        )
+        return(invisible(x$msg))
+      }
+    },
+
+    get_threshold = function(
+      ml
+    ){
+      threshold <- self$threshold %||% ml$get_threshold()
+      if(is.character(threshold)){
+        ml$unlabel_levels(threshold)
+      }
+      assert_valid_threshold(
+        threshold,
+        ml$get_log_levels(),
+        sprintf("Illegal threshold set for appender <%s>: ", class(self)[[1]])
+      )
+      threshold
+    },
+    threshold = NULL,
+    formatter = NULL,
+    format = NULL,
+    timestamp_format = NULL,
+    colors = NULL
+  )
+)
+
+
+
+
+#' @export
+console_appender_minimal <- appender_console_minimal$new()
+
+
+
+
+#' @export
+console_appender_color <- appender_console$new(
   colors = list(
     "fatal" = function(x) colt::clt_error(colt::clt_emph2(x)),
     "error" = colt::clt_error,
