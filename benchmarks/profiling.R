f723584a2b49bf31303588b4e0f70efe04da33bb
Stefan Fleck <stefan.b.fleck@gmail.com>
2018-10-26 23:53:50 +0200

    added futile.logger for comparison

--- benchmarks/profiling.R
+++ benchmarks/profiling.R
@@ -1,10 +1,35 @@
 library(memlog)
+library(futile.logger)
+
+# Benchmarking different appenders ----------------------------------------
+
+times <- list()
+
+ml_min <- memlog$new(appenders = console_appender_minimal)
+ml_std <- memlog$new(appenders = appender_console$new())
+ml_col <- memlog$new(appenders = console_appender_color)
+
+ml_min$fatal("blubb")
+ml_std$fatal("blubb")
+ml_col$fatal("blubb")
 
 
-ml <- memlog$new()
 sink("/dev/null")
-for (i in 1:1e5) ml$fatal("blubb")
+
+times[[1]] <- bench::system_time({for (i in 1:1e4) flog.info("blubb")})         # 15.20 s
+times[[2]] <- bench::system_time({for (i in 1:1e4) ml_min$fatal("blubb")})      #  4.17 s
+times[[3]] <- bench::system_time({for (i in 1:1e4) ml_std$fatal("blubb")})      #  7.28 s
+times[[4]] <- bench::system_time({for (i in 1:1e4) ml_col$fatal("blubb")})      #  12.9 s
+
+
 sink()
+
+
+times
+
+
+
+
 
 formatting
 ml$showdt()
