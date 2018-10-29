8484699f0a9d9dc76512c5198ddae607a48aca03
Stefan Fleck <stefan.fleck@statistik.gv.at>
2018-10-29 16:29:04 +0100

    set colors on load if colt is installed

--- R/memlog-package.R
+++ R/memlog-package.R
@@ -1,3 +1,22 @@
 #' @keywords internal
 #' @importFrom data.table data.table set
 "_PACKAGE"
+
+
+
+.onLoad <- function(...){
+
+  if (requireNamespace("colt", quietly = TRUE)){
+    options(
+      memlog.colors = list(
+        "fatal" = function(x) colt::clt_emph2(colt::clt_error(x)),
+        "error" = colt::clt_error,
+        "warn"  = colt::clt_warning,
+        "debug" = colt::clt_chr,
+        "trace" = colt::clt_chr
+      )
+    )
+  }
+
+  assign("ml", Memlog$new(), envir =  asNamespace("memlog"))
+}
