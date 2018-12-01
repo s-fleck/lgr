1149506e8f3bc57da666fd424758c5ad51a87e99
Stefan Fleck <stefan.b.fleck@gmail.com>
2018-12-01 08:50:46 +0100

    moved style functions to .onLoad where they belong

--- R/yog-package.R
+++ R/yog-package.R
@@ -2,6 +2,8 @@
 #'
 #' @section Options:
 #'
+#' You can also set these options in your `.Rprofile` to make them permanent
+#'
 #' \describe{
 #'   \item{`yog.colors`}{a `list` of `functions` used for coloring the log
 #'     levels in console output. Usually these will be functions from the
@@ -24,39 +26,71 @@
 
 #' @export yog
 .onLoad <- function(...){
+
+  # options -----------------------------------------------------------------
+  op <- options()
+  op.this <- list()
+
+
+
+  # +- colors ------------------------------------------------------------------
   if (requireNamespace("crayon", quietly = TRUE)){
 
-    options(
-      yog.colors = list(
-        "fatal" = style_fatal,
-        "error" = style_error,
-        "warn"  = style_warning,
-        "debug" = style_subtle,
-        "trace" = style_subtle
-      )
+    style_error   <- crayon::make_style("#BB3333", colors = 256)
+    style_fatal   <- function(x) style_error(crayon::bold(x))
+    style_warning <- crayon::make_style("#EEBB50", colors = 256)
+    style_subtle  <- crayon::make_style(grDevices::grey(0.5), grey = TRUE)
+    style_accent  <- crayon::silver
+    col_nchar     <- crayon::col_nchar
+
+    op.this[["yog.colors"]] = list(
+      "fatal" = style_fatal,
+      "error" = style_error,
+      "warn"  = style_warning,
+      "debug" = style_subtle,
+      "trace" = style_subtle
     )
+
   } else {
-    options(yog.colors = list())
+    style_fatal   <- function(...) paste(...)
+    style_error   <- style_fatal
+    style_warning <- style_fatal
+    style_subtle  <- style_fatal
+    style_accent  <- style_fatal
+    col_nchar     <- function(...) nchar(...)
+
+    op.this[["yog.colors"]] = list()
   }
 
-  options(
-    yog.log_levels = c(
-      "fatal" = 100L,
-      "error" = 200L,
-      "warn"  = 300L,
-      "info"  = 400L,
-      "debug" = 500L,
-      "trace" = 600L
-    )
+  assign("style_fatal", style_fatal, envir = parent.env(environment()))
+  assign("style_error", style_fatal, envir = parent.env(environment()))
+  assign("style_warning", style_fatal, envir = parent.env(environment()))
+  assign("style_subtle", style_fatal, envir = parent.env(environment()))
+  assign("style_accent", style_fatal, envir = parent.env(environment()))
+  assign("col_nchar", style_fatal, envir = parent.env(environment()))
+
+
+  # +- log_levels --------------------------------------------------------------
+  op.this[["yog.log_levels"]] <- c(
+    "fatal" = 100L,
+    "error" = 200L,
+    "warn"  = 300L,
+    "info"  = 400L,
+    "debug" = 500L,
+    "trace" = 600L
   )
 
-  appenders <- list(
-    console = AppenderConsole$new()
-  )
 
-  if (requireNamespace("data.table", quietly = TRUE)){
+  # +- set options -------------------------------------------------------------
+    toset <- !(names(op.this) %in% names(op))
+    if(any(toset)) options(op.this[toset])
+
+
+
+  # root looger -------------------------------------------------------------
+  appenders <- list(console = AppenderConsole$new())
+  if (requireNamespace("data.table", quietly = TRUE))
     appenders[["memory"]] <- AppenderMemoryDt$new()
-  }
 
   assign(
     "yog",
