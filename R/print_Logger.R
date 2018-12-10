9c6991a5933cd5d5d5c869b8b06e3736f9d617fd
Stefan Fleck <stefan.fleck@statistik.gv.at>
2018-12-10 10:45:55 +0100

    draft of a new and simpler print method for Loggers

---
+++ R/print_Logger.R
@@ -0,0 +1,266 @@
+print.Logger <- function(x){
+  cat(format(x), sep = "\n")
+}
+
+
+format.Logger = function(
+  x,
+  ...
+){
+  x <- yog
+
+  res <- list(
+    ancestry = x$ancestry,
+    appenders = x$appenders,
+    ancestral_appenders = x$ancestral_appenders
+  )
+
+  x$ancestry
+
+  header <- paste(
+    paste0("<", class(x)[[1]], ">"),
+    style_subtle(format(x$ancestry))
+  )
+
+  ind <- "  "
+
+  appenders <- do.call(rbind, lapply(x$appenders, srs_appender))
+  appenders$name <- rownames(appenders)
+  appenders$destination <- ifelse(
+    !is_blank(appenders$destination),
+    paste("->", appenders$destination),
+    ""
+  )
+
+  appenders <- with(
+    appenders,
+    paste0(
+      pad_right(name), ": ",
+      pad_right(class), " [",
+      pad_left(fmt_threshold(threshold, type = "character")), "] ",
+      destination
+    )
+  )
+
+  c(header, appenders)
+}
+
+
+
+summary.Appender = function(
+  x,
+  ...,
+  colors = TRUE
+){
+  ind <- "  "
+
+  header <- paste(
+    class(self)[[1]],
+    style_subtle(class_fmt(self, c("R6", class(self)[[1]])))
+  )
+
+  # Object summaries
+  obs    <- object_summaries(self)
+  methods <- obs[grep("function", obs)]
+  methods <- methods[!names(methods) %in% c("clone", "initialize", "print")]
+
+
+  active <- obs[obs == "active binding"]
+  active_bindings <- lapply(names(active), function(.x){
+    sv <- private[[paste0(".", .x)]]
+    iv <- self[[.x]]
+    if (!is.null(sv))
+      return(sls(sv))
+    else if (!is.null(iv))
+      return(paste(style_accent(sls(iv)), style_subtle("(inherited)")))
+    else
+      return(style_subtle("NULL"))
+  })
+  names(active_bindings) <- names(active)
+
+  # print
+
+  paste0(
+    paste0(header, "\n"),
+    paste0(ind, "Active Bindings:\n"),
+    paste0(ind, ind, names(active_bindings), ": ", active_bindings, collapse = "\n"), "\n",
+    paste0(ind, "Methods:\n"),
+    paste0(ind, ind, names(methods), ": ", methods, collapse = "\n")
+  )
+}
+
+
+
+
+# format appenders --------------------------------------------------------
+
+
+
+format_appenders <- function(
+  x
+){
+  res <- matrix(
+    dimnames = list(NULL, c("names", "threshold", "destination")),
+    nrow = length(x),
+    ncol = 3
+  )
+  if (length(x) > 0){
+    res[, "names"] <- pad_right(names(x))
+    res[, "threshold"] <- vapply(x, function(.x) format_threshold(.x$threshold), character(1))
+    res[, "destination"] <- vapply(x, function(.x) .x$destination, character(1))
+
+
+    appenders <- paste0(
+      pad_right(appenders$name), ": ",
+      pad_right(
+        paste0(label_levels(appenders$threshold), style_subtle(paste0(" (", appenders$threshold, ")")))
+      ),
+      vapply(appenders$comment, ptrunc_col, character(1), width = 128)
+    )
+  } else {
+    appenders <- style_subtle("none")
+  }
+
+  anc_appenders <- do.call(
+    rbind,
+    lapply(self$ancestral_appenders, function(.x){
+      cbind(data.frame(logger = .x$logger$name, srs(.x)))
+    }
+    ))
+
+
+  if (length(anc_appenders) > 0){
+    anc_appenders$logger <- pad_right(anc_appenders$logger)
+    anc_appenders$name   <- pad_right(anc_appenders$name)
+
+    anc_appenders <- paste0(
+      style_subtle(paste0(anc_appenders$logger, " -> ")),
+      anc_appenders$name, ": ",
+      pad_right(
+        paste0(label_levels(anc_appenders$threshold), style_subtle(paste0(" (", anc_appenders$threshold, ")")))
+      ),
+      vapply(anc_appenders$comment, ptrunc_col, character(1), width = 128)
+    )
+  } else {
+    anc_appenders <- NULL
+  }
+}
+
+
+
+
+fmt_threshold <- function(
+  x,
+  type = "both",
+  log_levels = getOption("yog.log_levels")
+){
+  assert(all(is.na(x)) || is_integerish(stats::na.omit(x)) || is.character(x))
+
+  log_levels = c("off" = 0L, log_levels)
+  if (is.character(x)){
+    assert(all(x %in% names(log_levels)))
+    x <- unlabel_levels(x, log_levels = log_levels)
+  }
+
+  impl <- function(.x){
+    assert((length(.x) == 1L) && (is.na(.x)) || is_integerish(.x))
+
+    if (.x %in% log_levels){
+      .r <- log_levels[which(log_levels == .x)]
+
+    } else if (is.na(.x)){
+      .r <- c("all" = NA)
+    } else {
+      return(format(.x))
+    }
+
+    if (identical(type, "character")){
+      return(names(.r))
+    }
+
+    paste0(names(.r), " (", .r, ")")
+  }
+
+  vapply(x, impl, character(1))
+}
+
+
+
+srs_appender <- function(x){
+  data.frame(
+    class = class_fmt(x, ignore = c("R6", "Filterable", "Appender")),
+    threshold = x$threshold,
+    destination = destination(x)
+  )
+}
+
+
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+destination <- function(x){
+  UseMethod("destination")
+}
+
+
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+destination.default <- function(x){
+  "(unknown)"
+}
+
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+destination.AppenderConsole <- function(x){
+  "console"
+}
+
+
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+destination.AppenderFile <- function(x){
+  browser()
+  x$file
+}
+
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+destination.AppenderMemoryDt <- function(x){
+  "memory"
+}
+
+
