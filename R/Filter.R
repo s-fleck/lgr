701ea8664a5eff426b30d450a921a629917c55e9
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-01-08 11:35:59 +0100

    Fleshed out formal Filter class and rewrote filter() methods to take a single `event` object

--- R/Filter.R
+++ R/Filter.R
@@ -1,56 +1,118 @@
-#'  Filter
+#'  Event Filters
 #'
-#'  @name Filter
+#'  Filters can be used for the `$set_filter()` and `$add_filter()` methods of
+#'  Appenders and Loggers. You normaly do not need to construct a formal
+#'  `EventFilter` object, you can just use any `function` that has the single
+#'  argument `event` or any object that has a `filter` method.
+#'
+#'  lgr comes with a few preset filters that do not actually filter LogEvents
+#'  but modify them. That is useful if you want to modify all logging cals for
+#'  a certain scope (usually a `function`)
+#'
+#'  \describe{
+#'
+#'  \item{`FilterInject$new(..., .list)`}{`...` and `.list` can take any number
+#'    of named R6 objects that will be injected as custom fields into all
+#'    LogEvents processed by the  Appender/Logger that this filter is attached
+#'    to. See also [with_log_value()]}
+#'
+#'  \item{`FilterForceLevel$new(level)`}{Sets the level of all LogEvents
+#'    processed by the Appender/Logger that this filter is attached to to
+#'    `level`. See also [with_log_value()]}
+#'  }
+#'
+#' @note
+#' The base class for Filters is called `EventFilter` so that it doesn't
+#' conflict with [base::Filter()]. The recommended convention for Filter
+#' subclasses is to call them `FilterSomething` and leave out the
+#' `Event` prefix.
+#'
+#'
+#' @section Accessing Appenders and Loggers from Filters:
+#'
+#' You can use the special function `.obj()` to access the calling
+#' Logger/Appender from within a filter
+#'
+#'
+#' @aliases Filter FilterInject FilterForceLevel
+#' @name EventFilter
+#'
+#' @examples
+#' # using filters to modify log events
+#' analyse <- function(){
+#'   lgr$add_filter(FilterForceLevel$new("info"), "force")
+#'   lgr$add_filter(FilterInject$new(type = "analysis"), "inject")
+#'   on.exit(lgr$remove_filter(c("force", "inject")))
+#'   lgr$debug("a debug message")
+#'   lgr$error("an error")
+#' }
+#' analyse()
+#' lgr$error("an error")
+#'
+#'
+#' # using .obj()
+#' l <- Logger$new("foo", threshold = "debug")
+#' f <- function(event) {
+#'   cat("via event$logger:", event$logger$threshold, "\n")  #  works for loggers only
+#'   cat("via .obj():      ",.obj()$threshold, "\n") # works for loggers and appenders
+#'   TRUE
+#' }
+#' l$add_filter(f)
+#' l$fatal("test")
+NULL
 
 
-
-#'  @export
-Filter <- R6::R6Class(
-  "Filter",
+#' @export
+EventFilter <- R6::R6Class(
+  "EventFilter",
   public = list(
-    filter = function(event, obj){TRUE}
+    initialize = function(fun = function(event){TRUE}){
+      self$filter <- fun
+    },
+    filter = NULL
   )
 )
 
-
-
+#' @export
 FilterInject <- R6::R6Class(
   "FilterInject",
-  inherit = Filter,
+  inherit = EventFilter,
   public = list(
     initialize = function(..., .list = list()){
       vals <- list(...)
       assert(!is.null(names(vals)) && is_equal_length(names(vals)), vals)
-      if (length(list) > 0){
+      if (length(.list) > 0){
         assert(!is.null(names(.list)) && is_equal_length(names(.list)), .list)
       }
       vals <- c(vals, .list)
       assert(all_are_distinct(names(vals)))
       self[["values"]] <- vals
-    },
-    filter = function(event, obj){
-      vals <- get("values", envir = self)
-      for (i in seq_along(vals)){
-        event[[names(vals)[[i]] ]] <- vals[[i]]
+
+      self$filter <- function(event){
+        vals <- get("values", envir = self)
+        for (i in seq_along(vals)){
+          event[[names(vals)[[i]] ]] <- vals[[i]]
+        }
+        TRUE
       }
-      TRUE
     },
     values = NULL
   )
 )
 
 
-
+#' @export
 FilterForceLevel <- R6::R6Class(
   "FilterForceLevel",
-  inherit = Filter,
+  inherit = EventFilter,
   public = list(
     initialize = function(level){
       self[["level"]] <- standardize_log_level(level)
-    },
-    filter = function(event, obj){
-      event[["level"]]  <- get("level", self)
-      TRUE
+
+      self[["filter"]] <- function(event){
+        event[["level"]]  <- get("level", self)
+        TRUE
+      }
     },
     level = NULL
   )
@@ -58,10 +120,29 @@ FilterForceLevel <- R6::R6Class(
 
 
 
+#' Check if an R Object is a Filter
+#'
+#' @param x any \R Object
+#' @seealso [EventFilter]
+#'
+#' @export
 is_filter <- function(
   x
 ){
-  inherits(x, "Filter") ||
-  (is.function(x) && identical(names(formals(x)), c("event", "obj"))) ||
-  (("filter" %in% names(x)) && is.function(x[["filter"]]) && is_filter(x[["filter"]]))
+  if (is.function(x)){
+    identical(names(formals(x)), c("event"))
+  } else {
+    # the extra is.function is to prevent infinite recursions
+    "filter" %in% names(x) && is.function(x[["filter"]]) && is_filter(x[["filter"]])
+  }
 }
+
+
+
+
+#' @export
+#' @rdname Filterable
+.obj <- function(){
+  get("self", parent.env(parent.frame(2)))
+}
+
