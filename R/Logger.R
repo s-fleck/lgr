803de44c3ac19b39d7157ea23dc6ebc13044f80b
Stefan Fleck <stefan.fleck@statistik.gv.at>
2019-05-03 13:08:01 +0200

    $config(NULL) works again as documented

--- R/Logger.R
+++ R/Logger.R
@@ -511,7 +511,6 @@ Logger <- R6::R6Class(
       }
 
       assert(is_parsed_logger_config(cfg))
-
       self$set_threshold(cfg$threshold)
       self$set_appenders(cfg$appenders)
       self$set_propagate(cfg$propagate)
@@ -888,7 +887,6 @@ LoggerRoot <- R6::R6Class(
       file,
       text
     ){
-
       if (is.null(cfg)){
         cfg <- logger_config()
         cfg$threshold <- getOption("lgr.default_threshold", 400L)
