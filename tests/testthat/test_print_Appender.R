7d531b143091ed6d703c4cd82e3cfc468e0038b3
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-06-01 11:15:02 +0200

    added expectiation to print.Appender tests

--- tests/testthat/test_print_Appender.R
+++ tests/testthat/test_print_Appender.R
@@ -3,31 +3,37 @@ context("print_Appender")
 
 
 
-test_that("print_Appender works as expected", {
+test_that("all Appenders print() without failure", {
   tf <- tempfile()
   on.exit(unlink(tf))
 
-  Appender$new()
-  AppenderConsole$new()
-  AppenderConsole$new(layout = LayoutGlue$new())
-  AppenderDbi$new(RSQLite::dbConnect(RSQLite::SQLite()), "blubb")
-  AppenderMemory$new()
-  AppenderMail$new()
-  AppenderSendmail$new("test@blah.com", control = list())
-  AppenderTable$new()
-  AppenderBuffer$new(appenders = list(
-    AppenderConsole$new(),
-    blah = AppenderBuffer$new()
-  ))
-  AppenderPushbullet$new(recipients = "blubb")
-  AppenderFileRotating$new(tf)
-  AppenderFileRotating$new(tf, size = "1kb")
-  app <- AppenderFileRotatingDate$new(tf, layout = LayoutJson$new())
-  app$rotate(force = TRUE)
-  app
+  expect_output({
+    print(Appender$new())
+    print(AppenderConsole$new())
+    print(AppenderConsole$new(layout = LayoutGlue$new()))
+    print(AppenderDbi$new(RSQLite::dbConnect(RSQLite::SQLite()), "blubb"))
+    print(AppenderMemory$new())
+    print(AppenderMail$new())
+    print(AppenderSendmail$new("test@blah.com", control = list()))
+    print(AppenderTable$new())
+    print(AppenderBuffer$new(appenders = list(
+      AppenderConsole$new(),
+      blah = AppenderBuffer$new()
+    )))
+    print(AppenderPushbullet$new(recipients = "blubb"))
+    print(AppenderFileRotating$new(tf))
+    print(AppenderFileRotating$new(tf, size = "1kb"))
+    app <- AppenderFileRotatingDate$new(tf, layout = LayoutJson$new())
+    app$rotate(force = TRUE)
+    print(app)
+    app$prune(0)
 
-
-  AppenderFileRotatingTime$new(tf, layout = LayoutJson$new(), age = "2 years", size = 90000)
-  AppenderFile$new(tf)
-
+    print(AppenderFileRotatingTime$new(
+      tf,
+      layout = LayoutJson$new(),
+      age = "2 years",
+      size = 90000)
+    )
+    print(AppenderFile$new(tf))
+  })
 })
