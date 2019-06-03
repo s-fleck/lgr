0b93cfe0501815a33607b4b778e5f8c20d7c8744
Stefan Fleck <stefan.fleck@statistik.gv.at>
2019-06-03 15:06:05 +0200

    implements algorithm to construct cli::tree compatible data.frame from loggers environment

--- R/logger_tree.R
+++ R/logger_tree.R
@@ -2,19 +2,38 @@ logger_tree <- function(
   x
 ){
   names <- ls(envir = loggers)
-
   nodes <- lapply(names, function(.x) unlist(strsplit(.x, "/")))
-  max_depth <- max(seq_len(vapply(nodes, length, integer(1))))
 
-  res <- list()
+  second_tier <- vapply(nodes, `[[`, FUN.VALUE = character(1), 1)
 
-  for (node in nodes){
-    for (i in seq_along(node)){
-      ancestry <- node[1:i]
-      nm <- node[[i]]
-      res[[nm]] <- list()
+  res <- data.frame(
+    parents = "root",
+    children = I(list(unique(second_tier))),
+    stringsAsFactors = FALSE
+  )
+
+
+  for (i in seq_along(nodes)){
+    for (j in seq_along(nodes[[i]])){
+      parent <- nodes[[i]][[j]]
+
+      if (j < length(nodes[[i]])){
+        child  <- nodes[[i]][[j + 1]]
+      } else {
+        child <- NULL
+      }
+
+      if (parent %in% res$parent){
+        sel <- res$parent == parent
+        res$children[sel] <- I(list(unlist(unique(compact(c(res$children[sel], child))))))
+
+      } else {
+        res <- rbind(
+          res,
+          data.frame(parents = parent, children = I(list(child)))
+        )
+      }
     }
-
   }
 
   res
--- tests/testthat/test_logger_tree.R
+++ tests/testthat/test_logger_tree.R
@@ -3,6 +3,10 @@ context("logger_tree")
 
 test_that("logger_tree works as expected", {
   skip("skip")
-  x <- get_logger("blah/blubb/swhupp")
+  get_logger("blah/blubb/swhupp")
+  get_logger("blah/blubb/woop")
+
   logger_tree()
+
+  cli::tree(res, root = "root")
 })
