b6c070f3517a869a2f31cc2ca37bda424780cd31
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-06-03 18:37:14 +0200

    implements algorithm that constructs cli::tree compatible data.frame from loggers environment

--- R/logger_tree.R
+++ R/logger_tree.R
@@ -2,20 +2,54 @@ logger_tree <- function(
   x
 ){
   names <- ls(envir = loggers)
-
   nodes <- lapply(names, function(.x) unlist(strsplit(.x, "/")))
-  max_depth <- max(seq_len(vapply(nodes, length, integer(1))))
+  max_depth <- seq_len(max(vapply(nodes, length, integer(1))))
+  second_tier <- setdiff(vapply(nodes, `[[`, character(1), 1L), "root")
 
-  res <- list()
+  res <- data.frame(
+    parent = "root",
+    children = I(list(unique(second_tier))),
+    stringsAsFactors = FALSE
+  )
 
-  for (node in nodes){
-    for (i in seq_along(node)){
-      ancestry <- node[1:i]
-      nm <- node[[i]]
-      res[[nm]] <- list()
+
+  for (i in seq_along(nodes)){
+    for (j in seq_along(nodes[[i]])){
+      parent_cur <- nodes[[i]][[j]]
+
+      if (j < length(nodes[[i]])){
+        child_cur  <- nodes[[i]][[j + 1]]
+      } else {
+        child_cur <- NULL
+      }
+
+      if (parent_cur %in% res$parent){
+        sel <- res$parent == parent_cur
+        res$children[sel] <- I(list(unlist(unique(compact(c(res$children[sel], child_cur))))))
+
+      } else {
+        res <- rbind(
+          res,
+          data.frame(parent = parent_cur, children = I(list(child_cur)))
+        )
+      }
     }
-
   }
 
-  res
+
+  assert(all_are_distinct(res$parent))
+
+  structure(
+    res,
+    class = union("logger_tree", class(res))
+  )
+}
+
+
+
+
+print.logger_tree <- function(x, ...){
+  assert_namespace("cli")
+  print(cli::tree(x))
+  x
 }
--- tests/testthat/test_logger_tree.R
+++ tests/testthat/test_logger_tree.R
@@ -1,8 +1,14 @@
 context("logger_tree")
 
 
+testthat::setup(remove_all_loggers())
+testthat::teardown(remove_all_loggers())
+
+
 test_that("logger_tree works as expected", {
-  skip("skip")
-  x <- get_logger("blah/blubb/swhupp")
-  logger_tree()
+  get_logger("blah/blubb/foo")
+  get_logger("blah/blubb/bar")
+  expect_setequal(logger_tree()$parent, c("root", "blah", "blubb", "foo", "bar"))
+  remove_all_loggers()
+  expect_identical(logger_tree()$parent, "root")
 })
