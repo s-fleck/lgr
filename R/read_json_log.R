366691c97026ef75352cbee125d79eb99780352f
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-12-15 11:57:15 +0100

    read_json_lines() passes ... dot jsonlite::stream_in

--- R/read_json_log.R
+++ R/read_json_log.R
@@ -1,16 +1,16 @@
 #' Read a JSON logfile
 #'
 #' @param file `character` scalar. path to a JSON logfile (one JSON object per line)
-#'
+#' @param ... passed on to [jsonlite::steam_in()]
 #' @return a `data.frame`
 #' @seealso [LayoutJson]
 #' @export
 #'
-read_json_lines <- function(file){
+read_json_lines <- function(file, ...){
   assert_namespace("data.table", "jsonlite")
   con <- file(file, open = "r")
   on.exit(close(con))
-  res <- jsonlite::stream_in(con, verbose = FALSE)
+  res <- jsonlite::stream_in(con, verbose = FALSE, ...)
 
   if ("timestamp" %in% names(res)){
     res$timestamp <- as.POSIXct(res$timestamp)
