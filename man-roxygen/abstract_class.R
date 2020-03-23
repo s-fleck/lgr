50ee3d08b8c26646f2176d2ddc829298d0d3e53c
Stefan Fleck <stefan.fleck@statistik.gv.at>
2020-03-23 11:14:20 +0100

    Initial gh-pages commit

--- man-roxygen/abstract_class.R
+++
@@ -1,5 +0,0 @@
-#' @description
-#' This is an **abstract class** and as such cannot be instantiated directly.
-#' It is exported for package developers that want to extend it
-#'
-#' @family abstract classes
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(lgr)
-
-test_check("lgr")
