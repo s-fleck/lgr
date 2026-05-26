5c9224219593afb3267763cb2496e030a8a71b08
stefan-fleck <stefan.fleck@wikifolio.com>
2026-05-26 11:37:51 +0200

    more fixes to config tests

--- tests/testthat/test_config_filters.R
+++ tests/testthat/test_config_filters.R
@@ -1,3 +1,21 @@
+config_with_file_fixture <- function(config_file, env = parent.frame()) {
+
+  basic_config(console = "info")
+
+  lgr$config(file = config_file)
+
+  withr::defer({
+    basic_config()
+    for (l in logger_index()$name){
+      get_logger(l, reset = TRUE)
+    }
+  },
+  envir = env)
+
+  invisible()
+}
+
+
 test_that("filters are configured and applied correctly", {
   skip_if_not_installed("yaml")
 
@@ -6,7 +24,7 @@ test_that("filters are configured and applied correctly", {
     "config_comprehensive.yaml"
   )
 
-  lgr$config(file = config_file)
+  config_with_file_fixture(config_file)
 
   # Root logger should have no filters
   expect_length(lgr$filters, 0)
@@ -35,7 +53,7 @@ test_that("filters from JSON config work correctly", {
     "config_filters.json"
   )
 
-  lgr$config(file = config_file)
+  config_with_file_fixture(config_file)
 
   # myapp logger should have error_level filter
   myapp <- get_logger("myapp")
@@ -59,7 +77,7 @@ test_that("FilterForceLevel properties are accessible", {
     "config_comprehensive.yaml"
   )
 
-  lgr$config(file = config_file)
+  config_with_file_fixture(config_file)
 
   app_db <- get_logger("app/db")
   filter_obj <- app_db$filters[[1]]
@@ -76,9 +94,42 @@ test_that("FilterForceLevel properties are accessible", {
 })
 
 
+
+
+test_that("logger - with FilterForceLevel - overrides log level", {
+  skip_if_not_installed("yaml")
+
+
+  # arrange
+  config_with_file_fixture(config_file)
+
+  lgr$config(file = config_file)
+
+  testing_force_error <- get_logger("testing/force_error_level")
+
+  # act
+  capture.output(testing_force_error$log(level = 400L, msg = "test message"))
+  events <- testing_force_error$last_event
+
+  # assert
+  expect_identical(testing_force_error$threshold, 500L)  # debug
+  expect_length(testing_force_error$filters, 1)
+  filter_obj <- testing_force_error$filters[[1]]
+
+  expect_identical(filter_obj$level, 200L)
+  expect_identical(events$level, 200L)
+})
+
+
+
+
 test_that("logger thresholds and filters interact correctly", {
   skip_if_not_installed("yaml")
 
+  # Cleanup
+  config_with_file_fixture(config_file)
+
+  # arrange
   config_file <- rprojroot::find_testthat_root_file(
     "testdata",
     "config_appenders.yaml"
@@ -86,25 +137,30 @@ test_that("logger thresholds and filters interact correctly", {
 
   lgr$config(file = config_file)
 
-  testing <- get_logger("testing")
 
-  # Testing logger has debug threshold and trace_only filter
-  expect_identical(testing$threshold, 500L)  # debug
-  expect_length(testing$filters, 1)
+  # act
+  testing_force_error <- get_logger("testing/force_error_level")
+  testing_force_trace <- get_logger("testing/force_trace_level")
+  testing_force_error$info("test message that should be promoted to error")
+  testing_force_trace$info("test message that should be demoted to trace")
 
-  filter_obj <- testing$filters[[1]]
-  expect_identical(filter_obj$level, 600L)  # trace
+  # loggers are correctly configured
+  expect_identical(testing_force_error$threshold, 500L)
+  expect_identical(testing_force_trace$threshold, 500L)
+  expect_length(testing_force_error$filters, 1)
+  expect_length(testing_force_trace$filters, 1)
+
+  expect_identical(testing_force_trace$filters[[1]]$level, 600L)
+  expect_identical(testing_force_error$filters[[1]]$level, 200L)
 
   # Verify logging behavior
   # Events at trace level should be captured by the filter
-  testing$log(level = 600L, msg = "test trace message")
-  events <- testing$last_event
 
-  expect_identical(events$level, 600L)
+  # filters are applied after the threshold, so the 600 (trace) event is
+  # *not* filtered out despite the loggers threshold being 500 (debug)
+  trace_event <- testing_force_trace$last_event
+  expect_identical(error_event$level, 600L)
 
-  # Cleanup
-  lgr$config(NULL)
-  get_logger("testing")$config(NULL)
-  get_logger("testing/unit")$config(NULL)
-  get_logger("testing/integration")$config(NULL)
+  error_event <- testing_force_error$last_event
+  expect_identical(error_event$level, 200L)
 })
